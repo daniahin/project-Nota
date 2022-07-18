801c7068c28aad2b3d0058c33e9cc67ace5e8391
daniahin <vlad.dubrovski20@gmail.com>
2022-07-18 17:44:43 +0300

    migrate

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.blog.models import Article, BlogCategory
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(BlogCategory)
+
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=256)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория блога'
+        verbose_name_plural = 'Категории блога'
+
+
 class Article(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     text_preview = models.TextField(verbose_name='Текст-превью')
     text = models.TextField(verbose_name='Текст')
-    created_at = models.DateTimeFiel(verbose_name='Дата создания', auto_now_add=True)
-    updated_at = models.DateTimeFiel(verbose_name='Дата создания', auto_now=True)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
