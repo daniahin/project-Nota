2420a53174ed375a704e5be02d3f8685da670e4b
daniahin <vlad.dubrovski20@gmail.com>
2022-07-27 11:17:24 +0300

    Html-Web commit

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -11,6 +11,15 @@ class BlogCategory(models.Model):
         verbose_name = 'Категория блога'
         verbose_name_plural = 'Категории блога'
 
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=256)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
 
 class Article(models.Model):
     category = models.ForeignKey(
@@ -19,6 +28,7 @@ class Article(models.Model):
         on_delete=models.SET_NULL,
         null=True
     )
+    tags = models.ManyToManyField(to=Tag, verbose_name='Тэги', blank=True)
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     text_preview = models.TextField(verbose_name='Текст-превью')
     text = models.TextField(verbose_name='Текст')
