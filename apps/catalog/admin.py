bb4a45ee795c0d7af231736c013640ae2b9168d8
daniahin <vlad.dubrovski20@gmail.com>
2022-08-15 18:19:13 +0300

    Product Category

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from apps.catalog.models import Category, Product
 
 
-admin.site.register(Product)
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
+
+
+class ProductCategoryInline(admin.TabularInline):
+    model = Product.categories.through
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'quantity', 'price']
+    inlines = [ProductCategoryInline]
+
+
